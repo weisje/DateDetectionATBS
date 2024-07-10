517cbfcd78cade106ef511ff485eea378344c75e
weisje <20269574+weisje@users.noreply.github.com>
2024-07-10 07:23:21 -0400

    imported regex library. Framed main() function.  Added initalizer to call main() function. Added dateFinder() function frame to house regex that will search for date canidates.  Added dateValidator() function to check if provided value is a valid date.

---
+++ dateDetection.py
@@ -0,0 +1,17 @@
+import re
+
+
+def dateFinder(): # TODO: Function to use regex to find potential dates in provided string
+    pass
+
+
+def dateValidator(): # TODO: Function to check provided string & determine if it is a valid date for a Gregorian calendar
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
