20da82377d3c5038938780febac03d2ad054f6e8
weisje <20269574+weisje@users.noreply.github.com>
2024-07-11 07:56:36 -0400

    Added ability to type in date when the program is called by name. Added string line for testing so the date doesn't have to continuously have to be typed in.  Put in body for dateValidator() function.  Added check for months that have thirty days as well as calculation to check for leap years & their wackiness.  Added final print to display date if it is valid. Replaced testing code with request for data.

--- dateDetection.py
+++ dateDetection.py
@@ -1,6 +1,7 @@
 import re
 import sys
 
+
 def dateFinder(possibleDateStr) -> tuple:
     """
     Uses regex to find potential calendary dates in provided string in format DD/MM/YYYY format.  Leading zero must be
@@ -14,17 +15,58 @@ def dateFinder(possibleDateStr) -> tuple:
     if date:
         return date.groups()
 
-def dateValidator(): # TODO: Function to check provided string & determine if it is a valid date for a Gregorian calendar
-    pass
+
+def dateValidator(dateToValidate) -> None:
+    """
+    Tests provided value to see if it is a valid date by the Gregorian calendar
+    :param dateToValidate: Value to see if it is actually a valid date
+    :type dateToValidate: tuple
+    :return: None
+    """
+    day = dateToValidate[0]
+    month = dateToValidate[1]
+    year = dateToValidate[2]
+    if month == "04" or month == "06" or month == "09" or month == "11":
+        if int(day) > 30:
+            sys.exit(f"Invalid date! {month} only has 30 days in it!")
+
+    elif month == "02":
+        if int(year) % 4 == 0:
+            if int(year) % 100 == 0:
+                if int(year) % 400 == 0:
+                    if int(day) > 29:
+                        sys.exit(f"Invalid date! {month} only has 29 days in it on goofy leap years!")
+                else:
+                    if int(day) > 28:
+                        sys.exit(f"Invalid date! {month} only has 28 days in it on leap years divisible by 100!")
+        else:
+            if int(day) > 28:
+                sys.exit(f"Invalid date! {month} only has 28 days in it on non-leap years!")
+
+    else:
+        if int(day) > 31:
+            sys.exit(f"{month} only has 31 days in it!")
+
+    print(f"{day}/{month}/{year} is a valid date")
 
 
-def main():
-    finderResult = dateFinder("31/01/2899")
+def main(dateToTest) -> None:
+    """
+    Main function for finding & testing viability of dates in provided string
+    :param dateToTest: value to be tested for a potential date
+    :type dateToTest: str
+    :return: None
+    """
+    if dateToTest == "":
+        sys.exit("Please enter data to test for a date")
+    finderResult = dateFinder(dateToTest)
     if not finderResult:
         sys.exit("No possible dates found")
     else:
         print(finderResult)
+        dateValidator(finderResult)
 
 
 if __name__ == '__main__':
-    main()
+    stringToTest = input("String to test\n>")
+    main(stringToTest)
